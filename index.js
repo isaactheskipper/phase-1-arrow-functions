b751427fccc5e134a8128dfb669c4183945a4429
isaac <issaktoo2@gmail,com>
2024-10-14 19:25:36 +0300

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  const square = num => num * num;
+  
+  const add = (a, b) => a + b;
+  
+  console.log("Divide:", divide()); 
+  console.log("Square of 5:", square(5)); 
+  console.log("Add 3 and 4:", add(3, 4)); 
+  
